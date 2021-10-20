7923c380387e937b846c9eba3c2ea6f0e9e6c983
ahmadrosid <alahmadrosid@gmail.com>
2021-10-20 20:25:26 +0700

    feat: curly

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'tailwindcss/tailwind.css'
+import 'codemirror/lib/codemirror.css'
+import '../style/custom.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,88 @@
+import axios from 'axios'
 import Head from 'next/head'
+import { useRef, useState, useEffect } from 'react'
+import parseCurl from '../lib/parse-curl'
 
 export default function Home() {
+  const [response, setResponse] = useState(false)
+  const [cmDoc, setCodeMirror] = useState(null)
+
+  const codeMirrorRef = useRef()
+
+  useEffect(() => {
+    if (!cmDoc) {
+      require('codemirror/mode/shell/shell')
+      const CodeMirror = require('codemirror')
+      const instanceInput = CodeMirror.fromTextArea(codeMirrorRef.current, {
+        lineNumbers: false,
+        lineWrapping: true,
+        mode: "text/x-sh"
+      })
+      instanceInput.setSize("100%", "100%")
+      setCodeMirror(instanceInput)
+    }
+  }, [])
+
+  const clearTextArea = () => {
+    setResponse(false)
+    cmDoc.getDoc().setValue('')
+  }
+
+  const runRequest = (e) => {
+    e.preventDefault()
+    const source = cmDoc.getValue()
+    if (!source) {
+      setResponse(false)
+      return;
+    }
+
+    const request = parseCurl(source)
+    if (!request) {
+      setResponse({
+        error: "Failed to parse curl request!"
+      })
+      return;
+    }
+
+    axios(request)
+      .then(response => setResponse(response))
+      .catch(err => {
+        setResponse({ err: err.message, response: err.response?.data })
+      })
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="w-full flex justify-center">
       <Head>
-        <title>Create Next App</title>
+        <title>Curl - Run curl request from browser</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="space-y-10 mt-[100px] pb-8">
+        <div className="text-center space-y-2">
+          <h1 className="text-gray-700 uppercase font-bold text-3xl">curly</h1>
+          <p className="text-gray-700">Run CURL Request from you browser.</p>
         </div>
-      </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <div className="w-full flex justify-center gap-x-6">
+          <button onClick={runRequest} className="bg-gray-500 rounded-md py-2 shadow-md w-[120px] text-white">Run</button>
+          <button onClick={clearTextArea} className="rounded-md py-2 shadow-md w-[120px] text-gray-800 border-2 border-gray-500">Clear</button>
+        </div>
+
+        <div>
+          <div className="h-[150px] w-[700px] text-white">
+            <textarea ref={codeMirrorRef}></textarea>
+          </div>
+        </div>
+
+        {response && (
+          <div className="bg-gray-800 rounded-md max-w-[700px]">
+            <pre className="p-4 text-white">
+              {JSON.stringify(response, null, '    ')}
+            </pre>
+          </div>
+        )}
+      </main>
     </div>
   )
 }
